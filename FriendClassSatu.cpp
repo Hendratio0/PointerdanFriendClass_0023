90f8f8412e6cb363beda5da33e14049d349508e2
Hendratio0 <hendraaatio@gmail.com>
2025-05-05 11:16:37 +0700

    commit11

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -22,3 +22,26 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << Id << endl
+         << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    Id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
